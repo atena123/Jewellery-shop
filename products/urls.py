9103ed10df55cc6a7a28018a31a11cde8a7df615
aneta <atena771@wp.pl>
2019-11-12 21:33:01 +0000

    add views, urls to products app

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, product_detail, product_filter
 
 
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^(?P<pk>\d+)/$', product_detail, name="product-detail"),
+    url(r'(?P<category>\w+)/$', product_filter, name="product-filter"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
     products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
+    
+def product_detail(request, pk):
+    products = get_object_or_404(Product, pk=pk)
+    return render(request, "productdetail.html", {"products": products})
+    
+def product_filter(request, category):
+    products = Product.objects.filter(category=category)
     return render(request, "products.html", {"products": products})
