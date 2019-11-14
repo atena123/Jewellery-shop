097861c62a4b42d0e7bf1130d271d2381cef367a
aneta <atena771@wp.pl>
2019-11-14 17:21:30 +0000

    create checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    state = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+        
+        
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.product.name, self.product.price)
