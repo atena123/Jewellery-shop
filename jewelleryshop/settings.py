3e68a725c9787a210d13d9bd3867335dab507fb2
aneta <atena771@wp.pl>
2019-11-14 16:50:33 +0000

    create checkout app

--- jewelleryshop/settings.py
+++ jewelleryshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,7 +44,8 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'search',
-    'contact'
+    'contact',
+    'checkout'
     
 ]
 
@@ -140,5 +142,8 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv("PUBLISHABLE_KEY")
+STRIPE_SECRET = os.getenv("STRIPE_SECRET")
 
 MESSAGE_STORAGE ="django.contrib.messages.storage.session.SessionStorage"
