350f523cb98a6753d39e1f4fd1aaf136c1a6b6d0
aneta <atena771@wp.pl>
2019-11-15 00:51:46 +0000

    add images to home page

--- jewelleryshop/settings.py
+++ jewelleryshop/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STRIPE_PUBLISHABLE = os.getenv("PUBLISHABLE_KEY")
-STRIPE_SECRET = os.getenv("STRIPE_SECRET")
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE ="django.contrib.messages.storage.session.SessionStorage"
